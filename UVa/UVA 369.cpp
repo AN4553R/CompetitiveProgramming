b5fc7b58b23e49aa1bba283fabc4d9137a67ac81
Abdelrahman Naser <abdonaser_97@outlook.com>
2019-04-15 00:31:15 +0200

    Create UVA 369.cpp

---
+++ UVa/UVA 369.cpp
@@ -0,0 +1,63 @@
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define _CRT_SECURE_NO_WaRNINGS
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+const double PI = acos(-1.0);
+const double EPS = (1e-9);
+typedef long long ll;
+typedef unsigned long long ull;
+#define flush fflush(stdout), cout.flush()
+#define all(v) ((v).begin()),((v).end())
+#define rall(v) ((v).rbegin()),((v).rend())
+#define clr(v,line) memset(v,line,sizeof(v))
+#define rep(i,n) for(int(i)=0;(i)<(int)(n);(i)++)
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define lcm(a,b) (a*(b/__gcd(a,b)))
+#define P 1000000007
+#define endl '\n'
+
+const ll dx[] = {1,-1,0,0,1,-1,1,-1};
+const ll dy[] = {0,0,1,-1,1,-1,-1,1};
+
+void file() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+void fast()
+{
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+
+ll dp[1000][1000];
+ll nCr(int n, int r)
+{
+       if(n==r) return dp[n][r] = 1;
+       if(r==0) return dp[n][r] = 1;
+       if(r==1) return dp[n][r] = n;
+       if(dp[n][r]) return dp[n][r];
+       return dp[n][r] = nCr(n-1,r) + nCr(n-1,r-1);
+}
+
+int main(int argc, char const *argv[])
+{
+	file();
+
+	ll x,y;
+
+	clr(dp,0);
+	
+	while (cin>>x>>y && x && y){
+
+		cout<<x<<" things taken "<<y<<" at a time is "<<nCr(x,y)<<" exactly."<<endl;
+	}
+}
